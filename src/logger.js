2ad6099f029e015e9d2ede1756333ee7965edd94
Filipe Salviano <filipesalviano@proton.me>
2024-12-12 17:07:57 -0300

    Logger modifications

--- src/logger.js
+++ src/logger.js
@@ -1,24 +1,38 @@
 const winston = require('winston');
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
-const logDirectory = path.join(__dirname, './../logs')
+const logDirectory = path.join(__dirname, './../logs');
 
 if (!fs.existsSync(logDirectory)) {
-    fs.mkdirSync(logDirectory)
+    fs.mkdirSync(logDirectory);
 }
- 
+
 const logger = winston.createLogger({
     format: winston.format.combine(
         winston.format.errors({ stack: true }),
+        winston.format.timestamp(),
         winston.format.json()
     ),
     transports: [
-        // new winston.transports.Console({ format: winston.combine(winston.colorize()) }),
-        new winston.transports.File({ filename: path.join(logDirectory, "error.log"), level: 'error' }),
-        new winston.transports.File({ filename: path.join(logDirectory, "app.log"), level: 'info' }),
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.timestamp(),
+                winston.format.printf(({ timestamp, level, message, stack }) => {
+                    return `${timestamp} [${level}]: ${stack || message}`;
+                })
+            )
+        }),
+        new winston.transports.File({
+            filename: path.join(logDirectory, "error.log"),
+            level: 'error'
+        }),
+        new winston.transports.File({
+            filename: path.join(logDirectory, "app.log"),
+            level: 'info'
+        }),
     ],
 });
- 
- 
+
 module.exports = logger;
