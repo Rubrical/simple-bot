abbc1f49763e54115cb0a985cf9aabe6e305e3cf
Filipe Salviano <filipesalviano@proton.me>
2024-12-21 13:01:44 -0300

    mention all command fully funcional

--- src/commands/mention-all.js
+++ src/commands/mention-all.js
@@ -1,36 +1,39 @@
 const logger = require('./../logger')
 
 module.exports.execute = async (client, flag, arg, M) => {
-    let groupData = ''
+    if (!M.isGroup) return
+
     let mentionMessage = ''
+    const groupData = await client.groupMetadata(M.from)
     const admNumber = M.sender.split('@')[0]
     const admPushName = M.pushName
     const whoMentioned = `Menção do ADM ${admPushName} @${admNumber}`
-
-    if (M.isGroup) groupData = await client.groupMetadata(M.from);
-
-    const members = groupData.participants || []
-    // const allMembersNumbers = 
+    const members = groupData.participants.map(x => x.id) || []
     
-    if (M.message.conversation && M.message.conversation.startsWith('/totag')) {
+    if (M.message.conversation && M.message.conversation.startsWith('/totag')) 
+    {
         mentionMessage = M.message.conversation.replace('/totag', '').trim()
-    } else if (M.message) {
+    } 
+    else if (M.message) 
+    {
         mentionMessage = M.quoted.text || M.quoted.message.conversation || undefined
     }
 
-    if (!mentionMessage) {
+    if (!mentionMessage || mentionMessage.trim().length === 0) 
+    {
         M.reply('🟥 *Mencione uma mensagem com seu aviso ou digite o comando com o aviso*')
     }
     
-    client.sendMessage(M.from, {
+    await client.sendMessage(M.from, 
+    {
         text: `${whoMentioned}\n${mentionMessage}`,
-        mentions: members.map(x => x.id)
+        mentions: members
     })
 }
 
 
 module.exports.command = {
-    name: 'mention everybody',
+    name: 'mention all',
     aliases: ['totag'],
     category: 'moderation',
     usage: '',
