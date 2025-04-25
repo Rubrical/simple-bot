49f3d7c5ff4ccd001cd93d8b66cb3d948d62619c
Filipe Salviano <filipesalviano@proton.me>
2025-04-25 03:03:42 -0300

    First method

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,12 +1,30 @@
+import logger from '../logger';
 import { api } from './../config/env-config';
 
 const url = "users";
+const routes = {
+    newUser: `${url}/new`,
+    incrementMessages: `${url}/update-message-quantity`,
+    incrementCommands: `${url}/update-command-quantity`,
+    getUser: `${url}/get-user`,
+    newAdmin: `${url}/admin`,
+    updateUser: `${url}/update-user`,
+}
 
 
 export const UsersService = {
-    newUser: async (user: UserRequest) => {},
-    newOwner: async () => {},
+    newUser: async (user: UserRequest) => {
+        return await api.post(routes.newUser, user)
+            .then((createdUser) => {
+                logger.info(`Novo usuário criado: ${JSON.stringify(createdUser)}`);
+                return true;
+            }).catch((err) => {
+                logger.warn(err.message);
+                return false;
+            });
+    },
     incrementMessages: async () => {},
+    incrementCommands: async () => {},
     userInfo: async () => {},
     newAdmin: async () => {},
     updateUser: async () => {},
@@ -27,4 +45,3 @@ export type User = {
     nome: string;
     tipoUsuario: number;
 }
-
